8d686f650f319b5648b7d2647237ad55f2b0a530
dipteshh <dmohanta_be20@thapar.edu>
2023-04-02 02:17:56 +0530

    updated api-base

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://todo-mern-backend-9q1n.onrender.com:3001";
 
 function App() {
   const [todos, setTodos] = useState([]);
